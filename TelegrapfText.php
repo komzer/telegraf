13f188d3a61a5ffe39f8f3a621aaab3bdf5065c0
karzov alex <umatna@gmail.com>
2023-04-21 09:37:41 +1000

    add function storeText

--- TelegrapfText.php
+++ TelegrapfText.php
@@ -2,6 +2,26 @@
 
 class TelegrapfText {
     public  $title, $text, $author, $publish, $slug;
+    function __construct($author , $slug )
+    {
+        $this -> author = $author;
+        $this -> slug = $slug;
+        $this -> publishdate =('Y-m-d H:i:s');
+    }
 
+    public function storeText($text, $title){
+        $arrayTelegrapf['text']= $this -> text =$text ;
+        $arrayTelegrapf['title']= $this -> title =$title;
+        $arrayTelegrapf['author']= $this -> author ;
+        $arrayTelegrapf['published']= $this -> published ;
+        $seria = serialize($arrayTelegrapf);
+        if ( file_exists($this->slug)){
+            $current = file_get_contents($this->slug);
+            $current .= $arrayTelegrapf;
+            file_put_contents($this->slug, $current);
+        } else{
+            file_put_contents($this->slug, $arrayTelegrapf);
+        }
+    }
 
 }
