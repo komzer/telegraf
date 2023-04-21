71b2b88bb4ef40ba9a8e64a368300e9268d7ef7b
karzov alex <umatna@gmail.com>
2023-04-21 16:11:05 +1000

    добавлены функуии loadText и editText

--- TelegrapfText.php
+++ TelegrapfText.php
@@ -6,22 +6,45 @@ class TelegrapfText {
     {
         $this -> author = $author;
         $this -> slug = $slug;
-        $this -> publishdate =('Y-m-d H:i:s');
+        $this -> publishdate = date('d.m.y - h:i:s');
     }
 
     public function storeText($text, $title){
-        $arrayTelegrapf['text']= $this -> text =$text ;
-        $arrayTelegrapf['title']= $this -> title =$title;
-        $arrayTelegrapf['author']= $this -> author ;
-        $arrayTelegrapf['published']= $this -> published ;
+        
+        $arrayTelegrapf['text']= $this -> text = $text ;
+        $arrayTelegrapf['title']= $this -> title = $title;
+        $arrayTelegrapf['author']= $this->author ;
+        $arrayTelegrapf['published']= $this -> published = date('d.m.y - h:i:s') ;
         $seria = serialize($arrayTelegrapf);
         if ( file_exists($this->slug)){
-            $current = file_get_contents($this->slug);
-            $current .= $arrayTelegrapf;
-            file_put_contents($this->slug, $current);
+           // $current = file_get_contents($this->slug);
+           // $current .= $arrayTelegrapf;
+            file_put_contents($this->slug,  $seria,  FILE_APPEND);
         } else{
-            file_put_contents($this->slug, $arrayTelegrapf);
+            file_put_contents($this->slug, $seria);
         }
     }
 
+    public function loadText( $slug ){
+        if ( file_exists($slug)){
+            $current = file_get_contents($this->slug);
+            $current = unserialize($current); 
+            $this -> text = $current['text'];
+            $this -> title = $current['title'];
+            $this -> author = $current['author'];
+            $this -> published = $current['published']; 
+            return ($this -> text);
+        }else{
+            echo 'нет такоего файла';
+        }
+    }
+    public function editText($title,$text){
+        $this -> text = $text ;
+        $this -> title = $title;
+    }
 }
+
+$d = new TelegrapfText('kod','vot.txt');
+
+$d -> storeText('chi-chi', 'ga-ga');
+$d -> loadText( 'vot');
